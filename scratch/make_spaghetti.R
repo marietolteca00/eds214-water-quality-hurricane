6648e768a749580ca3c27df87f71e86c8e335662
marietolteca00 <Marietolteca@gmail.com>
2025-08-26 16:00:03 -0700

    added comments and looked nice

--- scratch/make_spaghetti.R
+++ scratch/make_spaghetti.R
@@ -1,9 +1,13 @@
+
+# Loading in packages that may be used
 library(tidyverse)
 library(dplyr)
 library(janitor)
 library(here)
 library(zoo)
 
+
+# Reading in CSV files and assigning into dataframes
 brisley_one <- read_csv(here('data','QuebradaCuenca1-Bisley.csv')) %>% 
   clean_names()
 
@@ -17,40 +21,66 @@ prm <- read_csv(here('data','RioMameyesPuenteRoto.csv')) %>%
   clean_names() 
 
 
-puenterm <- prm %>% 
-  mutate(sample_date = lubridate::ymd(sample_date))
-
-#all_brisley <- brisley_one %>% 
-  # left_join(brisley_one, brisley_two, by= "sample_id", relationship = "many-to-many") %>% 
-  # left_join(brisley_two, by = "sample_id") %>% 
-  # left_join(brisley_three, by = "sample_id")
-
-#skimr::skim(all_brisley)
-
-half_brisley<- full_join(brisley_one, brisley_two)
-other_half<- full_join(brisley_three, prm)
-
-
 #use rbind instead of full join. This will allow each row to be treated individually instead of a key
-all_brisley <- rbind(brisley_one, brisley_two, brisley_two, prm)
+# Joining all csv's into one dataframe
+all_brisley <- rbind(brisley_one, brisley_two, brisley_three, prm)
 
-# all_brisley<- full_join(half_brisley, other_half) %>% 
-#   mutate(date = lubridate::ymd(sample_date),
-#          year= lubridate::year(sample_date))
+
+# ways to clean up dataframe with interested     
+all_brisley <- all_brisley %>% 
   
+  #Using mutate create a column called year, to have only the year
+  mutate(year = lubridate::year(sample_date)) %>% 
+  
+  #Using filter, grab the dates interested in before and after events
+  filter(year >= 1988 & year <= 1999) %>% 
+  
+  # Arrange by sample_date to have them in order
+  arrange(sample_date) %>% 
+  
+  # Remove these columns from the original dataframe
+  select(-don:-don_code) %>% 
+  
+  #group by sample_id
+  group_by(sample_id) %>%
+  
+  # Grab the running average on the interested chemicals
+  mutate(nh4_avg= rollmean(nh4_n, k= 1 , fill=NA)) %>%
+  mutate(ca_avg= rollmean(ca, k= 1 , fill=0)) %>%
+  mutate(mg_avg= rollmean(mg, k= 1, fill=0)) %>%
+  mutate(no3_avg= rollmean(no3_n, k= 9 , fill=0)) %>%
+  mutate(k_avg= rollmean(k, k= 1 , fill=0)) %>% 
+  mutate(ph_avg=rollmean(p_h, k=1, fill=0))
+  
+  #ungroup the group_by
+    #ungroup() %>% 
 
-#9weeks
+  
+  
+  
+# Creating plots to see if dataframe works and how it looks
+ggplot(data= all_brisley, aes(x= sample_date, y=k_avg ))+
+  geom_line(aes(color=sample_id))
+
+
+ggplot(data= all_brisley, aes(x= sample_date, y= nh4_avg)) +
+  geom_line(aes(color=sample_id))
+
+
+#9weeks breaks! 
 week_brisley <- all_brisley %>% 
   filter(year == "1989") %>% 
   filter(sample_date >= "1989-09-22",
-        sample_date <= "1989-10-31") %>% 
+        year <= "1994") %>% 
   arrange(sample_date) %>% 
   group_by(sample_date) %>% 
   mutate(average_mg= rollmean(mg, 9,fill=NA, align='right')) # stopped at rolling mean
 
-
+#DRAFT>>>
 ggplot(data=all_brisley, aes(x= sample_date, y=k )) +
-  geom_point()
+  geom_line()
+
+
 
 
 
